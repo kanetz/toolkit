8e6ff3398997a820897cfb8470aa37c19e413e5f
Your Name <you@example.com>
2014-05-22 11:42:22 +0800

    use pipeline history feed instead of cctray feed

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var program = require('commander'),
     url = require('url'),
     request = require('request'),
-    xml2js = require('xml2js'),
     exec = require('child_process').exec,
     logger = require('log4js').getLogger('main');
 
@@ -9,29 +8,23 @@ program
     .version('0.0.1')
     .option('-s, --server <server address>', 'Go Server Address', 'http://10.18.7.153:8153')
     .option('-p, --pipeline <pipeline name>', 'Pipeline Name', 'Tiger.Core')
-    .option('-i, --interval <interval in milliseconds>', 'Polling Interval', parseInt, 1000)
+    .option('-i, --interval <interval in milliseconds>', 'Polling Interval', parseInt, 500)
     .option('-r, --repository <repository path>', 'Repository Path', 'C:/Workspace/Tiger')
     .parse(process.argv);
 
-var cctrayUrl = url.resolve(program.server, '/go/cctray.xml');
-var pipelinePattern = new RegExp('^' + program.pipeline);
-logger.info('Retrieving pipeline status at', cctrayUrl);
+var pipelineHistoryFeedUrl = url.resolve(program.server, '/go/pipelineHistory.json?pipelineName=' + program.pipeline);
+logger.info('Retrieving pipeline history at', pipelineHistoryFeedUrl);
 watchPipeline();
 
 function watchPipeline() {
     'use strict';
-    request(cctrayUrl, cctrayFeedHandler);
+    request(pipelineHistoryFeedUrl, piplineHistoryFeedHandler);
 }
 
-function cctrayFeedHandler(error, response, body) {
+function piplineHistoryFeedHandler(error, response, body) {
     'use strict';
     if(!error && response.statusCode === 200) {
-        var parsingOptions = {
-            mergeAttrs: true,
-            normalizeTags: true,
-            explicitArray: false
-        };
-        xml2js.parseString(body, parsingOptions, xmlParsingHandler);
+        parsingHandler(JSON.parse(body.replace(/\\'/g, "'")));
     }
     else {
         logger.info('Error occurred, will try again in', program.interval, 'ms');
@@ -39,15 +32,12 @@ function cctrayFeedHandler(error, response, body) {
     }
 }
 
-function xmlParsingHandler(error, result) {
+function parsingHandler(pipeline) {
     'use strict';
 
-    var isPipelineLocked = result.projects.project
-        .some(function(project) {
-            return pipelinePattern.test(project.name) &&
-                (project.activity !== 'Sleeping' || project.lastBuildStatus !== 'Success');
-        });
-
+    var isPipelineLocked = pipeline.groups[0].history[0].stages.some(function(stage) {
+        return stage.stageStatus !== "Passed";
+    });
 
     if(isPipelineLocked) {
         logger.info('Pipeline is locked, will try again in', program.interval, 'ms');
